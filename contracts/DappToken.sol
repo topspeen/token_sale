7aa70acd0de87312f5095128a711485d3a1a6aca
topspeen <doberman.info@gmail.com>
2022-09-03 13:35:26 +0300

    2. Transfer tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,9 +1,34 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standart = "DApp Token v1.0";
+
+    event Transfer(
+
+         address indexed _from,
+         address indexed _to,
+         uint256 _value);
+
     uint256 public totalSupply;
 
-    constructor() public {
-        totalSupply = 1000000;
-    }
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+       }
+
+    function transfer(address _to, uint256 _value) public returns (bool succsess) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }   
+
 }
